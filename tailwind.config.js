9c543c846c0f715d42e2eed11771498454e94f09
Merik <merikreyimov@gmail.com>
2024-10-07 18:04:23 +0300

    tailwind fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", './utils/*.{html,js}'],
+  content: ["./index.html", "./login.html", './utils/*.{html,js}'],
   theme: {
     extend: {},
   },
